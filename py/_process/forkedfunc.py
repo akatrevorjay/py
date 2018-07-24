54b3dc01023f6b01f3ddb40dd1544fb1c14a13a2
Trevor Joynson (trevorj) <github@skywww.net>
2018-07-23 23:00:08 -0700

    Swap to attrs, allow to not capture stdout/stderr

--- py/_process/forkedfunc.py
+++ py/_process/forkedfunc.py
@@ -1,46 +1,68 @@
-
 """
     ForkedFunc provides a way to run a function in a forked process
     and get at its return value, stdout and stderr output as well
     as signals and exitstatusus.
 """
 
-import py
+import marshal
 import os
 import sys
-import marshal
+
+import attr
+import py
 
 
 def get_unbuffered_io(fd, filename):
     f = open(str(filename), "w")
     if fd != f.fileno():
         os.dup2(f.fileno(), fd)
+
     class AutoFlush:
+
         def write(self, data):
             f.write(data)
             f.flush()
+
         def __getattr__(self, name):
             return getattr(f, name)
+
     return AutoFlush()
 
 
+@attr.s
 class ForkedFunc:
     EXITSTATUS_EXCEPTION = 3
 
+    fun = attr.ib()
+    args = attr.ib(default=attr.Fac)
+    kwargs = attr.ib(default=None)
+    nice_level = attr.ib(default=0)
+    child_on_start = attr.ib(default=None)
+    child_on_exit = attr.ib(default=None)
+    capture_stderr = attr.ib(default=True)
+    capture_stdout = attr.ib(default=True)
 
-    def __init__(self, fun, args=None, kwargs=None, nice_level=0,
-                 child_on_start=None, child_on_exit=None):
-        if args is None:
-            args = []
-        if kwargs is None:
-            kwargs = {}
-        self.fun = fun
-        self.args = args
-        self.kwargs = kwargs
+    def __init__(
+            self,
+            fun,
+            args=None,
+            kwargs=None,
+            nice_level=0,
+            child_on_start=None,
+            child_on_exit=None
+            capture_stdout=True,
+            capture_stderr=True,
+    ):
+        pass
+
+    def __attrs_post_init__(self):
         self.tempdir = tempdir = py.path.local.mkdtemp()
         self.RETVAL = tempdir.ensure('retval')
-        self.STDOUT = tempdir.ensure('stdout')
-        self.STDERR = tempdir.ensure('stderr')
+
+        if self.capture_stdout:
+            self.STDOUT = tempdir.ensure('stdout')
+        if self.capture_stderr:
+            self.STDERR = tempdir.ensure('stderr')
 
         pid = os.fork()
         if pid:  # in parent process
@@ -49,31 +71,41 @@ class ForkedFunc:
             self.pid = None
             self._child(nice_level, child_on_start, child_on_exit)
 
-    def _child(self, nice_level, child_on_start, child_on_exit):
+    def _child(self):
         # right now we need to call a function, but first we need to
         # map all IO that might happen
-        sys.stdout = stdout = get_unbuffered_io(1, self.STDOUT)
-        sys.stderr = stderr = get_unbuffered_io(2, self.STDERR)
+        if self.capture_stdout:
+            sys.stdout = stdout = get_unbuffered_io(1, self.STDOUT)
+        if self.capture_stderr:
+            sys.stderr = stderr = get_unbuffered_io(2, self.STDERR)
+
         retvalf = self.RETVAL.open("wb")
         EXITSTATUS = 0
         try:
-            if nice_level:
-                os.nice(nice_level)
+            if self.nice_level:
+                os.nice(self.nice_level)
+
             try:
-                if child_on_start is not None:
-                    child_on_start()
+                if self.child_on_start is not None:
+                    self.child_on_start()
+
                 retval = self.fun(*self.args, **self.kwargs)
                 retvalf.write(marshal.dumps(retval))
-                if child_on_exit is not None:
-                    child_on_exit()
+
+                if self.child_on_exit is not None:
+                    self.child_on_exit()
             except:
                 excinfo = py.code.ExceptionInfo()
-                stderr.write(str(excinfo._getreprcrash()))
+                sys.stderr.write(str(excinfo._getreprcrash()))
                 EXITSTATUS = self.EXITSTATUS_EXCEPTION
+
         finally:
-            stdout.close()
-            stderr.close()
+            if self.capture_stdout:
+                stdout.close()
+            if self.capture_stderrd
+                stderr.close()
             retvalf.close()
+
         os.close(1)
         os.close(2)
         os._exit(EXITSTATUS)
@@ -112,6 +144,7 @@ class ForkedFunc:
 
 
 class Result(object):
+
     def __init__(self, exitstatus, signal, retval, stdout, stderr):
         self.exitstatus = exitstatus
         self.signal = signal
